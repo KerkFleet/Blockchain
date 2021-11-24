fedee441af439f905fcf63d14d369c38f0a04c35
KerkFleet <48966159+KerkFleet@users.noreply.github.com>
2021-11-24 15:37:10 -0600

    Update Newsletter.sol

--- Examples/NewsleterExample/Newsletter.sol
+++ Examples/NewsleterExample/Newsletter.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-import "./Token.sol";
+import "./NewsletterExample-Token.sol";
 
 contract Newsletter {
     Token _token;
+    mapping (address => bool) public subscribed;
 
     constructor(Token token) {
         _token = token;
     }
 
     function subscribe() public {
-        _token.mint(msg.sender, 10000000000000000000);
+        require(subscribed[msg.sender] == false, "You have already subscribed to this newsletter!");
+        subscribed[msg.sender] = true;
+        _token.mint(msg.sender, 30000000000000000000);
     }
 }
