a46bd7fbb5fef789f0c3b8754ce28620c32add54
KerkFleet <48966159+KerkFleet@users.noreply.github.com>
2021-11-30 08:58:03 -0600

    Update SimpleNFT.sol

--- Examples/NFTs/SimpleNFT/SimpleNFT.sol
+++ Examples/NFTs/SimpleNFT/SimpleNFT.sol
@@ -6,18 +6,22 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract MyNFT is ERC721URIStorage {
 
     constructor(address owner) ERC721("Thor's Hammer", "HAMR") public {
-        _mint(owner, 1);
+        _safeMint(owner, 1);
     }
 
+    //override to set base uri
     function _baseURI() internal view virtual override returns(string memory) {
-        return "https://github.com/KerkFleet/Blockchain/blob/master/Examples/NFTs/";
+        // can use pinata to post data in a decentralized manner
+        return "https://github.com/KerkFleet/Blockchain/edit/master/Examples/NFTs/SimpleNFT/";
     }
 
+    //override to append json file name instead of tokeID since there is only one
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
         string memory baseURI = _baseURI();
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, "tokenURI.json")) : "";
     }
+
 
 }
