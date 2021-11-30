92410456060b75c6d01e5e2b7e2070e691141860
KerkFleet <48966159+KerkFleet@users.noreply.github.com>
2021-11-29 21:28:25 -0600

    Update SimpleNFT.sol

--- Examples/NFTs/SimpleNFT.sol
+++ Examples/NFTs/SimpleNFT.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage {
+
+    constructor(address owner) ERC721("Thor's Hammer", "HAMR") public {
+        _mint(owner, 1);
+    }
+
+    function _baseURI() internal view virtual override returns(string memory) {
+        return "https://github.com/KerkFleet/Blockchain/blob/master/Examples/NFTs/";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, "tokenURI.json")) : "";
+    }
+
+}
